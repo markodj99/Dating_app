648fac32adde5a7ca34e3bd8def11fe9c7ad0bc3
markodj99 <80117888+markodj99@users.noreply.github.com>
2025-09-02 18:45:40 +0200

    added users controller

---
+++ API/API/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController(AppDbContext _context) : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet("all")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user is null) return NotFound();
+            return Ok(user);
+        }
+    }
+}
