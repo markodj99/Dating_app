54b2ea1b0b4783940458428a3e29ef50441cbd63
markodj99 <80117888+markodj99@users.noreply.github.com>
2025-09-04 15:39:15 +0200

    little bit more of routing

--- Client/src/layout/navbar/navbar.ts
+++ Client/src/layout/navbar/navbar.ts
@@ -2,21 +2,24 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 import { LoginCreds } from '../../types/user';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   protected creds: LoginCreds = {} as LoginCreds;
   protected accountService = inject(AccountService);
+  private router = inject(Router);
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
       next: response => { 
-        console.log(response);
+        this.router.navigateByUrl('/users');
         this.creds = {} as LoginCreds;
       },
       error: error => alert(error.message)
@@ -25,6 +28,7 @@ export class Navbar {
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
 
