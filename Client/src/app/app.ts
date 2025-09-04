702ba3eb8f67ddef2a2193f838ff21512f52422b
markodj99 <80117888+markodj99@users.noreply.github.com>
2025-09-04 17:30:15 +0200

    route animations added

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,9 +1,5 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Navbar } from "../layout/navbar/navbar";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -12,35 +8,32 @@ import { Router, RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
+export class App {
   protected readonly title: string = 'Dating App';
   protected router = inject(Router);
-  protected users = signal<User[]>([]);
 
-  // constructor(private http: HttpClient) { } stari nacin za DI
-  private http = inject(HttpClient); // novi nacin za DI
-  private accountService = inject(AccountService);
-
-  async ngOnInit(): Promise<void> {
-    this.users.set(await this.getUsers()); // ako se bojimo da ne uradi unsubscribe
-    this.setCurrentUser();
-    // this.http.get('https://localhost:5001/api/user/all').subscribe({
-    //   next: response => this.users.set(response),
-    //   error: error => console.log(error),
-    //   complete: () => console.log('Request completed') // opcionalno
-    // });
-  }
-
-  async getUsers(): Promise<User[]> {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/user/all'));
-  } catch (error) {
-      console.log(error)
-      throw error;
-    }
-  }
-
-  setCurrentUser() {
-
-  }
 }
+
+
+
+  // ako nekad zatreba
+  // // constructor(private http: HttpClient) { } stari nacin za DI
+  // private http = inject(HttpClient); // novi nacin za DI
+
+  // async ngOnInit(): Promise<void> {
+  //   this.users.set(await this.getUsers()); // ako se bojimo da ne uradi unsubscribe
+  //   // this.http.get('https://localhost:5001/api/user/all').subscribe({
+  //   //   next: response => this.users.set(response),
+  //   //   error: error => console.log(error),
+  //   //   complete: () => console.log('Request completed') // opcionalno
+  //   // });
+  // }
+
+  // async getUsers(): Promise<User[]> {
+  //   try {
+  //     return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/user/all'));
+  // } catch (error) {
+  //     console.log(error)
+  //     throw error;
+  //   }
+  // }
