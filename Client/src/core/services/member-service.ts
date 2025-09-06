50de249ba9df062b31604503f8cc95320cb7153f
markodj99 <80117888+markodj99@users.noreply.github.com>
2025-09-06 13:27:09 +0200

    member detail components

--- Client/src/core/services/member-service.ts
+++ Client/src/core/services/member-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../../types/member';
+import { Photo } from '../../types/photo';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,9 @@ export class MemberService {
 
   getMember(id: string) {
     return this.http.get<Member>(this.baseUrl + 'member/' + id);
+  }
+
+  getMemberPhotos(id: string) {
+    return this.http.get<Photo[]>(this.baseUrl + 'member/' + id + '/photos');
   }
 }
